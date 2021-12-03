0e7819c193896ae34127cef6331ffe2d6aad6134
Mark-Grabowski <mark.walter.grabowski@gmail.com>
2021-12-03 17:52:09 +0000

    blouch R Package v.1.0.0

---
+++ R Package/blouch v1.0.0/R/blouch-package.R
@@ -0,0 +1,16 @@
+#' The 'blouch' package.
+#'
+#' @description A DESCRIPTION OF THE PACKAGE
+#'
+#' @docType package
+#' @name blouch-package
+#' @aliases blouch
+#' @useDynLib blouch, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#'
+#' @references
+#' Stan Development Team (NA). RStan: the R interface to Stan. R package version 2.26.4. https://mc-stan.org
+#'
+NULL
